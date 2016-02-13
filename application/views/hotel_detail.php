8d772fc12d46844f64f5d472643ce7177848f9f8
kyawminhtwe107 <kyawminhtwe107@gmail.com>
2016-02-14 00:46:03 +0630

    created slider and add file

--- application/views/hotel_detail.php
+++ application/views/hotel_detail.php
@@ -1,7 +1,8 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/hotel.css">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/hotel_detail.css">
+<script type="text/javascript" src="<?php echo base_url(); ?>js/jssor.slider.min.js"></script>
+<!-- use jssor.slider.debug.js instead for debug -->
 <div class="container-fluid" id="page_content">
-
   <div class="container">
 
   <!-- /hotel name and title -->
@@ -30,130 +31,93 @@
       <!-- /right content -->
       <div class="col-xs-12 col-sm-4 col-md-9 col-lg-9" id="right_content_detail">
 
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="right_inner"></div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="right_inner">
+          
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 800px; height: 456px; overflow: hidden; visibility: hidden; background-color: #24262e;">
+        <!-- Loading Screen -->
+        <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
+            <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
+            <div style="position:absolute;display:block;background:url('<?php echo base_url(); ?>img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
+        </div>
+        <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 800px; height: 356px; overflow: hidden;">
+            <div data-p="144.50" style="display: none;">
+                <img data-u="image" src="<?php echo base_url(); ?>img/01.jpg" />
+                <img data-u="thumb" src="<?php echo base_url(); ?>img/thumb-01.jpg" />
+            </div>
+            <div data-p="144.50" style="display: none;">
+                <img data-u="image" src="<?php echo base_url(); ?>img/02.jpg" />
+                <img data-u="thumb" src="<?php echo base_url(); ?>img/thumb-02.jpg" />
+            </div>
+            <div data-p="144.50" style="display: none;">
+                <img data-u="image" src="<?php echo base_url(); ?>img/03.jpg" />
+                <img data-u="thumb" src="<?php echo base_url(); ?>img/thumb-03.jpg" />
+            </div>
+            <div data-p="144.50" style="display: none;">
+                <img data-u="image" src="<?php echo base_url(); ?>img/04.jpg" />
+                <img data-u="thumb" src="<?php echo base_url(); ?>img/thumb-04.jpg" />
+            </div>
+            <div data-p="144.50" style="display: none;">
+                <img data-u="image" src="<?php echo base_url(); ?>img/05.jpg" />
+                <img data-u="thumb" src="<?php echo base_url(); ?>img/thumb-05.jpg" />
+            </div>
+            <div data-p="144.50" style="display: none;">
+                <img data-u="image" src="<?php echo base_url(); ?>img/06.jpg" />
+                <img data-u="thumb" src="<?php echo base_url(); ?>img/thumb-06.jpg" />
+            </div>
+            <div data-p="144.50" style="display: none;">
+                <img data-u="image" src="<?php echo base_url(); ?>img/07.jpg" />
+                <img data-u="thumb" src="<?php echo base_url(); ?>img/thumb-07.jpg" />
+            </div>
+            <div data-p="144.50" style="display: none;">
+                <img data-u="image" src="<?php echo base_url(); ?>img/08.jpg" />
+                <img data-u="thumb" src="<?php echo base_url(); ?>img/thumb-08.jpg" />
+            </div>
+            <div data-p="144.50" style="display: none;">
+                <img data-u="image" src="<?php echo base_url(); ?>img/09.jpg" />
+                <img data-u="thumb" src="<?php echo base_url(); ?>img/thumb-09.jpg" />
+            </div>
+            <div data-p="144.50" style="display: none;">
+                <img data-u="image" src="<?php echo base_url(); ?>img/10.jpg" />
+                <img data-u="thumb" src="<?php echo base_url(); ?>img/thumb-10.jpg" />
+            </div>
+            <div data-p="144.50" style="display: none;">
+                <img data-u="image" src="<?php echo base_url(); ?>img/11.jpg" />
+                <img data-u="thumb" src="<?php echo base_url(); ?>img/thumb-11.jpg" />
+            </div>
+            <div data-p="144.50" style="display: none;">
+                <img data-u="image" src="<?php echo base_url(); ?>img/12.jpg" />
+                <img data-u="thumb" src="<?php echo base_url(); ?>img/thumb-12.jpg" />
+            </div>
+        </div>
+        <!-- Thumbnail Navigator -->
+        <div data-u="thumbnavigator" class="jssort01" style="position:absolute;left:0px;bottom:0px;width:800px;height:100px;" data-autocenter="1">
+            <!-- Thumbnail Item Skin Begin -->
+            <div data-u="slides" style="cursor: default;">
+                <div data-u="prototype" class="p">
+                    <div class="w">
+                        <div data-u="thumbnailtemplate" class="t"></div>
+                    </div>
+                    <div class="c"></div>
+                </div>
+            </div>
+            <!-- Thumbnail Item Skin End -->
+        </div>
+        <!-- Arrow Navigator -->
+        <span data-u="arrowleft" class="jssora05l" style="top:158px;left:8px;width:40px;height:40px;"></span>
+        <span data-u="arrowright" class="jssora05r" style="top:158px;right:8px;width:40px;height:40px;"></span>
+        <a href="http://www.jssor.com" style="display:none">Slideshow Maker</a>
+    </div>
+    <script>
+        jssor_1_slider_init();
+    </script>
+          </div>
+
+        </div>
 
       </div><!-- /end right content -->
 
     </div><!-- // end hotel detail content section -->
 
-          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
-              <div class="col-md-12" id="left_menu_header">
-                <h4>PLACE AROUND YANGON STATE (MYANMAR)</h4>
-              </div>
-              <div class="col-md-12" id="left_menu">
-                <ul>
-                  
-                  <li><span class="glyphicon glyphicon-star"></span><span class="pull-right"></span></li>
-                
-                </ul>
-              </div>
-              <div class="col-md-12" id="left_menu">
-                <ul>
-                  <li><span class="glyphicon glyphicon-star"></span>Yangon <span class="pull-right">(235)</span></li>
-                  <li><span class="glyphicon glyphicon-star"></span>Saung Chaung<span class="pull-right">(235)</span></li>
-                  <li><span class="glyphicon glyphicon-star"></span>Dagon<span class="pull-right">(235)</span></li>
-                  <li><span class="glyphicon glyphicon-star"></span>Hledan<span class="pull-right">(235)</span></li>
-                  <li><span class="glyphicon glyphicon-star"></span>Sule<span class="pull-right">(235)</span></li>
-                  <li><span class="glyphicon glyphicon-star"></span>Pazun Taung<span class="pull-right">(235)</span></li>
-                  <li><span class="glyphicon glyphicon-star"></span>Thingan Gyun<span class="pull-right">(235)</span></li>
-                  <li><span class="glyphicon glyphicon-star"></span>Kyi Myin Taing<span class="pull-right">(235)</span></li>
-                  <li><span class="glyphicon glyphicon-star"></span>La Thar<span class="pull-right">(235)</span></li>
-                </ul>
-              </div>
-              <div class="col-md-12" id="left_menu_header">
-                <h4> PROPERTY TYPE</h4>
-              </div>
-              <div class="col-md-12" id="left_menu">
-                <ul>
-                  <li><span class="glyphicon glyphicon-star"></span>5 Star<span class="pull-right">(235)</span></li>
-                  <li><span class="glyphicon glyphicon-star"></span>4 Star<span class="pull-right">(235)</span></li>
-                  <li><span class="glyphicon glyphicon-star"></span>3 Star<span class="pull-right">(235)</span></li>
-                  <li><span class="glyphicon glyphicon-star"></span>2 Star<span class="pull-right">(235)</span></li>
-                  <li><span class="glyphicon glyphicon-star"></span>1 Star<span class="pull-right">(235)</span></li>
-                </ul>
-              </div>
-              <div class="col-md-12" id="left_menu_header">
-                <h4> FACILITIES</h4>
-              </div>
-              <div class="col-md-12" id="left_menu">
-                <ul>
-                  <li><span class="glyphicon glyphicon-star"></span>5 Star<span class="pull-right">(235)</span></li>
-                  <li><span class="glyphicon glyphicon-star"></span>4 Star<span class="pull-right">(235)</span></li>
-                  <li><span class="glyphicon glyphicon-star"></span>3 Star<span class="pull-right">(235)</span></li>
-                  <li><span class="glyphicon glyphicon-star"></span>2 Star<span class="pull-right">(235)</span></li>
-                  <li><span class="glyphicon glyphicon-star"></span>1 Star<span class="pull-right">(235)</span></li>
-                </ul>
-              </div>
-            </div>
-    <!-- /end left menu section -->
-    <!-- START RIGHT CONTENT SECTION -->
-            <div class="col-xs-12 col-sm-4 col-md-9 col-lg-9">
-              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="right_content">
-                <!-- map row -->
-                
-                <!-- /end map row -->
-                <!-- search box and nofification text section -->
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                  <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6" id="search_box_page">
-                  <form class="form">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="padding:0px;">
-                      <div class="input-group">
-                          <span class="input-group-addon" id="basic-addon1">In:</span>
-                          <input type="text" class="form-control" placeholder=" in-date" aria-describedby="basic-addon1">
-                      </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="padding:0px;">
-                        <div class="input-group">
-                            <span class="input-group-addon" id="basic-addon1">Out:</span>
-                            <input type="text" class="form-control" placeholder="out-date" aria-describedby="basic-addon1">
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="padding:0px;">
-                      <div class="col-sm-6" style="padding:0px;">
-                        <div class="input-group">
-                            <span class="input-group-addon" id="basic-addon1">Guests</span>
-                            <input type="number" class="form-control" placeholder="guest" aria-describedby="basic-addon1">
-                        </div>
-                      </div>
-                      <div class="col-sm-6" style="padding:0px;">
-                        <div class="input-group">
-                            <span class="input-group-addon" id="basic-addon1">Rooms</span>
-                            <input type="number" class="form-control" placeholder="room" aria-describedby="basic-addon1">
-                        </div>
-                      </div>                                
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                      <input type="submit" value="Search" name="search" class="btn btn-info pull-right">
-                    </div>
-                  </form>
-                  </div>
-                </div>
-                <!-- /end search box section -->
-              </div>
-
-            <!-- start content show -->
-              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="right_content_show">
-              
-                <div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
-                  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="one_item">
-                    <a href="">
-                    <img src="" class="img-responsive center-block">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="text">
-                      <small></small>
-                      <h4></h4>
-                      <p>
-                       Towering above the city centre competition, this new hotel has super smart rooms at an affordable price. Standard rooms are small but include many of the features found in the higher class rooms...
-                      </p>
-                    </div>
-                    </a>
-                  </div>
-                </div>
-               
-              </div>
-            <!-- /end content show -->
-            </div>
-            <!-- /end right content section -->
-          </div>
-        </div><!--/end container -->
+  </div><!--/end container -->
 </div>
